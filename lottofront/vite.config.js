d068726646574d966d991c9a2506c2a34ef1e67e
kidchang93 <lck930721@gmail.com>
2024-10-03 18:27:03 +0900

    CD: google cloud 자동 배포 연동

--- lottofront/vite.config.js
+++ lottofront/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server:{
     proxy: {
       "/api": {
-        target: "http://localhost:8080",
+        target: "http://172.17.0.2:8080",
         changeOrigin: true,
         secure: false,
         ws: true,
